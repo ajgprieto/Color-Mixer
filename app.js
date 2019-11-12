95df29995d5c95264e9563d779ae77da2cd8ead5
AJ Prieto <aj.garidanprieto@gmail.com>
2019-11-11 21:23:21 -1000

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const seedrandom = require('seedrandom');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 8080;
 
 function generateColorsWithSeed(seed) {
